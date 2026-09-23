4434023cac7a03b6afac895c83bf03b34442de2d
saheedajayi <saheed.abd.ajayi@gmail.com>
2026-09-23 22:45:12 +0100

    fix(web): add suppressHydrationWarning to html tag to ignore browser extension attributes

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -140,7 +140,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${generalSans.variable} ${neco.variable} ${playfair.variable} antialiased`}
       >
